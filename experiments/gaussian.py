f7bf771133becc08b081bc9ae352bca775a96f06
Wai-Shing Luk <luk036@gmail.com>
2025-01-29 15:02:14 +0800

    fix for android termux

--- experiments/gaussian.py
+++ experiments/gaussian.py
@@ -55,7 +55,7 @@ def plot_gp(mu, cov, X, X_train=None, Y_train=None, samples=[]):
     plt.fill_between(X, mu + uncertainty, mu - uncertainty, alpha=0.1)
     plt.plot(X, mu, label="Mean")
     for i, sample in enumerate(samples):
-        plt.plot(X, sample, lw=1, ls="--", label=f"Sample {i+1}")
+        plt.plot(X, sample, lw=1, ls="--", label=f"Sample {i + 1}")
     # plot observations if available
     if X_train is not None:
         plt.plot(X_train, Y_train, "rx")
--- tests/test_corr_bspline_fn.py
+++ tests/test_corr_bspline_fn.py
@@ -108,4 +108,4 @@ def test_lsq_corr_bspline2():
 def test_mle_corr_bspline():
     _, num_iters, feasible = mle_corr_bspline(Y, site, 4)
     assert feasible
-    assert num_iters <= 384
+    assert num_iters <= 388
